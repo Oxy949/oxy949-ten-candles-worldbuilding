cc7fb2c2e25b17b77bafd9e52947481c17d2b463
Konstantin Kustov <oxy949@gmail.com>
2024-09-17 21:24:13 +0300

    Style + rename

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,49 @@
-Hooks.once('init', async function() {
+// Import TypeScript modules
+import { registerSettings } from './lib/settings.js';
 
+/* ------------------------------------ */
+/* Initialize module					*/
+/* ------------------------------------ */
+Hooks.once('init', async function () {
+    console.log('Ten Candles | Initializing');
+    // Assign custom classes and constants here
+
+    // Register custom module settings
+    // Preload Handlebars templates
+    // await preloadTemplates();
+    // Register custom sheets (if any)
 });
 
-Hooks.once('ready', async function() {
+/* ------------------------------------ */
+/* Setup module							*/
+/* ------------------------------------ */
+Hooks.once('setup', function () {
+});
 
+/* ------------------------------------ */
+/* When ready							*/
+/* ------------------------------------ */
+Hooks.once('ready', function () {
+});
+
+Hooks.on("renderActorSheet", (app, html, data) => {
+  // Изменяем текст вкладки "Description" на "Описание"
+  html.find('a[data-tab="description"]').text("Описание");
+
+  // Изменяем текст вкладки "Items" на "Особенности"
+  html.find('a[data-tab="items"]').text("Особенности");
+
+  // Убираем вкладку "Attributes"
+  html.find('a[data-tab="attributes"]').remove();
+});
+
+Hooks.on("renderItemSheet", (app, html, data) => {
+  // Изменяем текст вкладки "Description" на "Описание"
+  //html.find('a[data-tab="description"]').text("Описание");
+
+  // Убираем вкладку "Attributes"
+  //html.find('a[data-tab="attributes"]').remove();
+
+  html.find('.sheet-tabs').css('visibility', 'hidden');
+  html.find('.sheet-tabs').css('position', 'absolute');
 });
